a482d59c28d07daddb5d2cef8377b9ba0751f24d
qyt <486179@qq.com>
2022-09-16 12:37:57 +0800

    modify:token close with authorize session

--- XEngine_Source/XEngine_APPService/XEngine_AuthorizeService/Authorize_Task.cpp
+++ XEngine_Source/XEngine_APPService/XEngine_AuthorizeService/Authorize_Task.cpp
@@ -29,6 +29,26 @@ void __stdcall XEngine_TaskEvent_Client(LPCSTR lpszUserAddr, LPCSTR lpszUserName
 }
 void __stdcall XEngine_TaskEvent_Token(XNETHANDLE xhToken, LPVOID lParam)
 {
+	AUTHREG_USERTABLE st_UserTable;
+	memset(&st_UserTable, '\0', sizeof(AUTHREG_USERTABLE));
+
+	Session_Token_Get(xhToken, &st_UserTable);
+	if (st_UserTable.st_UserInfo.nUserLevel > 0)
+	{
+		//如果权限是普通用户
+		AUTHREG_PROTOCOL_TIME st_TimeProtocol;
+		memset(&st_TimeProtocol, '\0', sizeof(AUTHREG_PROTOCOL_TIME));
+		//需要设置时间并且关闭会话
+		if (Session_Authorize_GetTimer(st_UserTable.st_UserInfo.tszUserName, &st_TimeProtocol))
+		{
+			Database_SQLite_UserLeave(&st_TimeProtocol);
+		}
+		Session_Authorize_CloseClient(st_UserTable.st_UserInfo.tszUserName);
+		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("Token：%lld，用户名：%s，离开服务器,在线时长:%d"), xhToken, st_UserTable.st_UserInfo.tszUserName, st_TimeProtocol.nTimeONLine);
+	}
+	else
+	{
+		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("Token:%lld,已经超时,被移除服务器"), xhToken);
+	}
 	Session_Token_Delete(xhToken);
-	XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("Token:%lld,已经超时,被移除服务器"), xhToken);
 }
