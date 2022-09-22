a53cd27b48c4aeb0214c797d26fbdae7a30972f3
qyt <486179@qq.com>
2022-09-22 13:48:26 +0800

    added:logout and timeout process code for http pass

--- XEngine_Source/XEngine_APPService/XEngine_AuthorizeService/AuthorizeHTTP_Post/AuthorizeHTTP_Pass.cpp
+++ XEngine_Source/XEngine_APPService/XEngine_AuthorizeService/AuthorizeHTTP_Post/AuthorizeHTTP_Pass.cpp
@@ -23,19 +23,42 @@ BOOL XEngine_AuthorizeHTTP_Pass(LPCTSTR lpszClientAddr, LPCTSTR lpszAPIName, LPC
 		{
 			Protocol_Packet_HttpComm(tszSDBuffer, &nSDLen, 404, "user notfound");
 			XEngine_Client_TaskSend(lpszClientAddr, tszSDBuffer, nSDLen, XENGINE_AUTH_APP_NETTYPE_HTTP);
-			XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_ERROR, _T("客户端：%s，用户名：%s，登录失败，用户名不存在"), lpszClientAddr, st_UserAuth.tszUserName);
+			XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_ERROR, _T("PASS客户端：%s，用户名：%s，登录失败，用户名不存在"), lpszClientAddr, st_UserAuth.tszUserName);
 			return FALSE;
 		}
 		if ((_tcslen(st_UserAuth.tszUserPass) != _tcslen(st_UserTable.st_UserInfo.tszUserPass)) || (0 != _tcsncmp(st_UserAuth.tszUserPass, st_UserTable.st_UserInfo.tszUserPass, _tcslen(st_UserTable.st_UserInfo.tszUserPass))))
 		{
 			Protocol_Packet_HttpComm(tszSDBuffer, &nSDLen, 400, "password is incorrent");
 			XEngine_Client_TaskSend(lpszClientAddr, tszSDBuffer, nSDLen, XENGINE_AUTH_APP_NETTYPE_HTTP);
-			XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_ERROR, _T("客户端：%s，用户名：%s，登录失败，密码错误"), lpszClientAddr, st_UserAuth.tszUserName);
+			XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_ERROR, _T("PASS客户端：%s，用户名：%s，登录失败，密码错误"), lpszClientAddr, st_UserAuth.tszUserName);
 			return FALSE;
 		}
 		Protocol_Packet_HttpClientInfo(tszSDBuffer, &nSDLen, &st_UserTable);
 		XEngine_Client_TaskSend(lpszClientAddr, tszSDBuffer, nSDLen, XENGINE_AUTH_APP_NETTYPE_HTTP);
-		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("HTTP客户端:%s,请求删除用户:%s 成功"), lpszClientAddr, st_UserAuth.tszUserName);
+		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("PASS客户端:%s,请求登录用户:%s 成功"), lpszClientAddr, st_UserAuth.tszUserName);
+	}
+	else if (0 == _tcsnicmp(lpszAPIName, lpszAPILogout, _tcslen(lpszAPIName)))
+	{
+		AUTHREG_PROTOCOL_TIME st_ProtocolTime;
+		memset(&st_ProtocolTime, '\0', sizeof(AUTHREG_PROTOCOL_TIME));
+
+		Protocol_Parse_HttpParseTime(lpszMsgBuffer, nMsgLen, &st_ProtocolTime);
+		Database_SQLite_UserLeave(&st_ProtocolTime);
+
+		Protocol_Packet_HttpComm(tszSDBuffer, &nSDLen);
+		XEngine_Client_TaskSend(lpszClientAddr, tszSDBuffer, nSDLen, XENGINE_AUTH_APP_NETTYPE_HTTP);
+		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("PASS客户端：%s，用户名：%s，请求离开服务成功"), lpszClientAddr, st_ProtocolTime.tszUserName);
+	}
+	else if (0 == _tcsnicmp(lpszAPIName, lpszAPITimeout, _tcslen(lpszAPIName)))
+	{
+		AUTHREG_PROTOCOL_TIME st_ProtocolTime;
+		memset(&st_ProtocolTime, '\0', sizeof(AUTHREG_PROTOCOL_TIME));
+
+		Protocol_Parse_HttpParseTime(lpszMsgBuffer, nMsgLen, &st_ProtocolTime);
+		Database_SQLite_UserLeave(&st_ProtocolTime);
+		Protocol_Packet_HttpComm(tszSDBuffer, &nSDLen);
+		XEngine_Client_TaskSend(lpszClientAddr, tszSDBuffer, nSDLen, XENGINE_AUTH_APP_NETTYPE_HTTP);
+		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("PASS客户端：%s，用户名：%s，超时通知成功"), lpszClientAddr, st_ProtocolTime.tszUserName);
 	}
 	return TRUE;
 }
