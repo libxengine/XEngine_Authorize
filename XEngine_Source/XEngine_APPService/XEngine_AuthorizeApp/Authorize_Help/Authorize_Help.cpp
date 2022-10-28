a1beff39bb832b05a10a66f09afb120da2e6a7d1
qyt <486179@qq.com>
2022-10-28 09:52:17 +0800

    fixed:not upload help code

---
+++ XEngine_Source/XEngine_APPService/XEngine_AuthorizeApp/Authorize_Help/Authorize_Help.cpp
@@ -0,0 +1,45 @@
+﻿#include "pch.h"
+#include "../resource.h"
+#include "Authorize_Help.h"
+/********************************************************************
+//    Created:     2022/09/15  09:40:05
+//    File Name:   H:\XEngine_Authorize\XEngine_Source\XEngine_APPService\XEngine_AuthorizeApp\Authorize_Help\Authorize_Help.cpp
+//    File Path:   H:\XEngine_Authorize\XEngine_Source\XEngine_APPService\XEngine_AuthorizeApp\Authorize_Help
+//    File Base:   Authorize_Help
+//    File Ext:    cpp
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     帮助函数类
+//    History:
+*********************************************************************/
+//////////////////////////////////////////////////////////////////////////
+//                           公有函数
+//////////////////////////////////////////////////////////////////////////
+BOOL Authorize_Help_LogPrint(LPCTSTR lpszLogBuffer)
+{
+	TCHAR tszLogBuffer[8196];
+	TCHAR tszAddBuffer[8196];
+	TCHAR tszTimeStr[64];
+
+	memset(tszLogBuffer, '\0', sizeof(tszLogBuffer));
+	memset(tszAddBuffer, '\0', sizeof(tszAddBuffer));
+	memset(tszTimeStr, '\0', sizeof(tszTimeStr));
+
+	::GetDlgItemText(hMainWnd, IDC_EDIT1, tszAddBuffer, sizeof(tszAddBuffer));
+	
+	BaseLib_OperatorTime_TimeToStr(tszTimeStr);
+	if (_tcslen(tszAddBuffer) <= 0)
+	{
+		_stprintf(tszLogBuffer, _T("%s:%s"), tszTimeStr, lpszLogBuffer);
+	}
+	else if (_tcslen(tszAddBuffer) >= 8196)
+	{
+		_stprintf(tszLogBuffer, _T("%s:%s"), tszTimeStr, lpszLogBuffer);
+	}
+	else
+	{
+		_stprintf(tszLogBuffer, _T("%s\r\n%s:%s"), tszAddBuffer, tszTimeStr, lpszLogBuffer);
+	}
+	::SetDlgItemText(hMainWnd, IDC_EDIT1, tszLogBuffer);
+	return TRUE;
+}
---
+++ XEngine_Source/XEngine_APPService/XEngine_AuthorizeApp/Authorize_Help/Authorize_Help.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+/********************************************************************
+//    Created:     2022/09/15  09:39:01
+//    File Name:   H:\XEngine_Authorize\XEngine_Source\XEngine_APPService\XEngine_AuthorizeApp\Authorize_Help\Authorize_Help.h
+//    File Path:   H:\XEngine_Authorize\XEngine_Source\XEngine_APPService\XEngine_AuthorizeApp\Authorize_Help
+//    File Base:   Authorize_Help
+//    File Ext:    h
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     帮助函数类
+//    History:
+*********************************************************************/
+BOOL Authorize_Help_LogPrint(LPCTSTR lpszLogBuffer);
