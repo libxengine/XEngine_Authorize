5ed9abfcc09b2e46f892709b9f7a72a87c04e2f6
qyt <486179@qq.com>
2022-12-29 15:11:55 +0800

    fixed:banned packet addr is incorrent

--- XEngine_Source/AuthorizeModule_Protocol/Protocol_Packet/Protocol_Packet.cpp
+++ XEngine_Source/AuthorizeModule_Protocol/Protocol_Packet/Protocol_Packet.cpp
@@ -650,13 +650,13 @@ BOOL CProtocol_Packet::Protocol_Packet_HttpBanned(TCHAR* ptszMsgBuffer, int* pIn
 		st_JsonObject["tszUserName"] = (*pppSt_BannedUser)[i]->tszUserName;
 		st_JsonUser.append(st_JsonObject);
 	}
-	for (int i = 0; i < nUserCount; i++)
+	for (int i = 0; i < nAddrCount; i++)
 	{
 		Json::Value st_JsonObject;
-		st_JsonObject["nID"] = (*pppSt_BannedUser)[i]->nID;
-		st_JsonObject["tszIPEnd"] = (*pppSt_BannedUser)[i]->tszIPEnd;
-		st_JsonObject["tszIPStart"] = (*pppSt_BannedUser)[i]->tszIPStart;
-		st_JsonObject["tszTime"] = (*pppSt_BannedUser)[i]->tszTime;
+		st_JsonObject["nID"] = (*pppSt_BannedAddr)[i]->nID;
+		st_JsonObject["tszIPEnd"] = (*pppSt_BannedAddr)[i]->tszIPEnd;
+		st_JsonObject["tszIPStart"] = (*pppSt_BannedAddr)[i]->tszIPStart;
+		st_JsonObject["tszTime"] = (*pppSt_BannedAddr)[i]->tszTime;
 		st_JsonAddr.append(st_JsonObject);
 	}
 	st_JsonRoot["msg"] = "success";
