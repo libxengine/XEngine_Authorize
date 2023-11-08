575206e4b05e473612448705e6c9cf3e2a289ef4
qyt <486179@qq.com>
2023-11-08 14:49:24 +0800

    modify:button push key for close service when service start is failed

--- XEngine_Source/XEngine_APPService/XEngine_AuthorizeService/XEngine_AuthorizeService.cpp
+++ XEngine_Source/XEngine_APPService/XEngine_AuthorizeService/XEngine_AuthorizeService.cpp
@@ -263,7 +263,7 @@ XENGINE_EXITAPP:
 
 	if (bIsRun)
 	{
-		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_WARN, _X("网络验证服务器退出..."));
+		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_WARN, _X("启动失败,按任意键网络验证服务器退出..."));
 		bIsRun = false;
 
 		HelpComponents_Datas_Destory(xhTCPPacket);
@@ -284,10 +284,10 @@ XENGINE_EXITAPP:
 		Session_Token_Destroy();
 		AuthHelp_DynamicCode_Destory();
 		Database_SQLite_Destroy();
-		exit(0);
 	}
 #ifdef _WINDOWS
 	WSACleanup();
 #endif
+	getchar();
 	return 0;
 }
