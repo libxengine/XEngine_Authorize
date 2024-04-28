a1bec91dffaa3e7b12368c75163e3a000f52af71
qyt <486179@qq.com>
2024-04-28 15:25:49 +0800

    added:info report module

---
+++ XEngine_Source/XEngine_ThirdPart/XEngine_InfoReport/InfoReport_Define.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+/********************************************************************
+//    Created:     2024/04/28  15:18:55
+//    File Name:   D:\XEngine_Authorize\XEngine_Source\XEngine_ThirdPart\XEngine_InfoReport\InfoReport_Define.h
+//    File Path:   D:\XEngine_Authorize\XEngine_Source\XEngine_ThirdPart\XEngine_InfoReport
+//    File Base:   InfoReport_Define
+//    File Ext:    h
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     信息报告导出定义
+//    History:
+*********************************************************************/
+//////////////////////////////////////////////////////////////////////////
+//                        导出函数
+//////////////////////////////////////////////////////////////////////////
+extern "C" XLONG InfoReport_GetLastError(int *pInt_ErrorCode = NULL);
+/************************************************************************/
+/*                        APIMachine报告                                */
+/************************************************************************/
+/********************************************************************
+函数名称：InfoReport_APIMachine_Send
+函数功能：发送一条信息报告给API服务器
+ 参数.一：lpszAPIUrl
+  In/Out：In
+  类型：常量字符指针
+  可空：N
+  意思：输入请求地址
+返回值
+  类型：逻辑型
+  意思：是否成功
+备注：lpszAPIUrl = _X("http://127.0.0.1:5501/api?function=machine&params1=0");
+*********************************************************************/
+extern "C" bool InfoReport_APIMachine_Send(LPCXSTR lpszAPIUrl);
+/********************************************************************
+函数名称：InfoReport_APIMachine_GetTime
+函数功能：获取报告次数
+ 参数.一：lpszAPIUrl
+  In/Out：In
+  类型：常量字符指针
+  可空：N
+  意思：输入请求地址
+ 参数.二：pInt_TimeNumber
+  In/Out：Out
+  类型：整数型指针
+  可空：N
+  意思：输出报告次数
+返回值
+  类型：逻辑型
+  意思：是否成功
+备注：
+*********************************************************************/
+extern "C" bool InfoReport_APIMachine_GetTime(LPCXSTR lpszAPIUrl, __int64x* pInt_TimeNumber);
---
+++ XEngine_Source/XEngine_ThirdPart/XEngine_InfoReport/pch.h
@@ -0,0 +1,69 @@
+﻿// pch.h: 这是预编译标头文件。
+// 下方列出的文件仅编译一次，提高了将来生成的生成性能。
+// 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
+// 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
+// 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
+
+#ifndef PCH_H
+#define PCH_H
+
+// 添加要在此处预编译的标头
+#ifdef _MSC_BUILD
+#include "framework.h"
+#include <tchar.h>
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
+#endif //PCH_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <json/json.h>
+#include <list>
+#include <unordered_map>
+#include <XEngine_Include/XEngine_CommHdr.h>
+#include <XEngine_Include/XEngine_Types.h>
+#include <XEngine_Include/XEngine_ProtocolHdr.h>
+#include <XEngine_Include/XEngine_BaseLib/BaseLib_Define.h>
+#include <XEngine_Include/XEngine_BaseLib/BaseLib_Error.h>
+#include <XEngine_Include/XEngine_SystemSdk/SystemApi_Define.h>
+#include <XEngine_Include/XEngine_SystemSdk/SystemApi_Error.h>
+#include <XEngine_Include/XEngine_Client/APIClient_Define.h>
+#include <XEngine_Include/XEngine_Client/APIClient_Error.h>
+#include "InfoReport_Define.h"
+#include "InfoReport_Error.h"
+/********************************************************************
+//    Created:     2024/04/28  15:16:49
+//    File Name:   D:\XEngine_Authorize\XEngine_Source\XEngine_ThirdPart\XEngine_InfoReport\pch.h
+//    File Path:   D:\XEngine_Authorize\XEngine_Source\XEngine_ThirdPart\XEngine_InfoReport
+//    File Base:   pch
+//    File Ext:    h
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     公用头
+//    History:
+*********************************************************************/
+extern bool InfoReport_IsErrorOccur;
+extern XLONG InfoReport_dwErrorCode;
+
+#ifdef _MSC_BUILD
+#pragma comment(lib,"XEngine_BaseLib/XEngine_BaseLib")
+#pragma comment(lib,"XEngine_SystemSdk/XEngine_SystemApi")
+#pragma comment(lib,"XEngine_Client/XClient_APIHelp")
+#ifdef _DEBUG
+#ifdef _WIN64
+#pragma comment(lib,"../../x64/Debug/jsoncpp")
+#else
+#pragma comment(lib,"../../Debug/jsoncpp")
+#endif
+#else
+#ifdef _WIN64
+#pragma comment(lib,"../../x64/Release/jsoncpp")
+#else
+#pragma comment(lib,"../../Release/jsoncpp")
+#endif
+#endif
+#endif
